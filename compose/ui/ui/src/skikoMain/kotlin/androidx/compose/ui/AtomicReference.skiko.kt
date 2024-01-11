994e1702750d83a7d7825b3a2c86bb7a72a0332c
Igor Demin <igor.demin@jetbrains.com>
2024-01-11 04:12:13 +0100

    Fix AtomicReference compilation
    marked internal, as it was internal in the jvm sourceSet, and we can safely mark in commonMain

--- compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/AtomicReference.skiko.kt
+++ compose/ui/ui/src/skikoMain/kotlin/androidx/compose/ui/AtomicReference.skiko.kt
@@ -16,6 +16,14 @@
 
 package androidx.compose.ui
 
-import androidx.compose.runtime.AtomicReference
+import kotlinx.atomicfu.atomic
 
-internal actual typealias AtomicReference<V> = AtomicReference<V>
+internal actual class AtomicReference<V> actual constructor(value: V) {
+    private val delegate = atomic(value)
+    actual fun get() = delegate.value
+    actual fun set(value: V) {
+        delegate.value = value
+    }
+    actual fun getAndSet(value: V) = delegate.getAndSet(value)
+    actual fun compareAndSet(expect: V, newValue: V) = delegate.compareAndSet(expect, newValue)
+}
