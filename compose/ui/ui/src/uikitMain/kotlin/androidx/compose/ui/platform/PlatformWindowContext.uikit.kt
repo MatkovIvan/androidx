MatkovIvan/androidx

commit 0ebc8d148990e7111d41c98b1f9e47c342433529

Revisit internal APIs to correctly handle coordinates conversion (#1117)
## Proposed Changes

- Replacing `ComposeScene.boundsInWindow` to `size` again
- Add `ComposeScene.invalidatePositionInWindow` to invalidate the
position of a compose scene in the window
- Introducing `calculateLocalPosition`/`calculatePositionInWindow` in
`PlatformContext` to calculate position in the window
- Unify px-dp-platform primitives conversion
- Changed only APIs that are marked as `@InternalComposeApi`

## Testing

Test: everything should work like before. CI should handle that.

compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/PlatformWindowContext.uikit.kt
@@ -16,33 +16,25 @@
 
 package androidx.compose.ui.platform
 
-import androidx.compose.ui.input.pointer.PointerKeyboardModifiers
-import androidx.compose.ui.toDpOffset
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.uikit.systemDensity
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.round
+import androidx.compose.ui.unit.asCGPoint
+import androidx.compose.ui.unit.asDpOffset
+import androidx.compose.ui.unit.toDpOffset
 import androidx.compose.ui.unit.toOffset
 import kotlinx.cinterop.cValue
 import kotlinx.cinterop.useContents
 import platform.UIKit.UIView
 
 /**
- * This class copied from Desktop sourceSet.
  * Tracking a state of window.
- *
- * TODO: Extract information about window from [PlatformContext] in skiko source set.
- *
  */
 internal class PlatformWindowContext {
     private val _windowInfo = WindowInfoImpl()
     val windowInfo: WindowInfo get() = _windowInfo
 
     private var _windowContainer: UIView? = null
-
-    fun setKeyboardModifiers(modifiers: PointerKeyboardModifiers) {
-        _windowInfo.keyboardModifiers = modifiers
-    }
 
     fun setWindowFocused(focused: Boolean) {
         _windowInfo.isWindowFocused = focused
@@ -53,29 +45,38 @@ internal class PlatformWindowContext {
     }
 
     fun setContainerSize(size: IntSize) {
-        if (_windowInfo.containerSize != size) {
-            _windowInfo.containerSize = size
-        }
+        _windowInfo.containerSize = size
     }
 
-    /**
-     * Calculates the offset of the given [container] within the window.
-     * It uses [_windowContainer] as a reference for window coordinate space.
-     *
-     * @param container The container component whose offset needs to be calculated.
-     * @return The offset of the container within the window as an [IntOffset] object.
-     */
-    fun offsetInWindow(container: UIView): IntOffset {
-        val density = container.systemDensity
-        return if (_windowContainer != null && _windowContainer != container) {
-            container.convertPoint(
-                point = cValue { container.bounds.useContents { origin } },
-                toView = _windowContainer,
-            )
+    fun calculatePositionInWindow(container: UIView, localPosition: Offset): Offset {
+        val windowContainer = _windowContainer ?: return localPosition
+        return convertPoint(
+            point = localPosition,
+            fromView = container,
+            toView = windowContainer
+        )
+    }
+
+    fun calculateLocalPosition(container: UIView, positionInWindow: Offset): Offset {
+        val windowContainer = _windowContainer ?: return positionInWindow
+        return convertPoint(
+            point = positionInWindow,
+            fromView = windowContainer,
+            toView = container
+        )
+    }
+
+    private fun convertPoint(point: Offset, fromView: UIView, toView: UIView): Offset {
+        return if (fromView != toView) {
+            val density = fromView.systemDensity
+            fromView.convertPoint(
+                point = cValue { point.toDpOffset(density).asCGPoint() },
+                toView = toView,
+            ).useContents {
+                asDpOffset().toOffset(density)
+            }
         } else {
-            cValue { container.bounds.useContents { origin } }
-        }.useContents {
-            toDpOffset().toOffset(density).round()
+            point
         }
     }
 }

compose/ui/ui-test/src/skikoMain/kotlin/androidx/compose/ui/test/Assertions.skikoMain.kt
@@ -74,14 +74,7 @@ internal actual fun SemanticsNode.isInScreenBounds(assertIsFullyVisible: Boolean
             nodeBoundsInWindow.bottom <= visibleBounds.bottom
     } else {
         // assertIsDisplayed only throws if the element is fully offscreen
-        !nodeBoundsInWindow.intersect(
-            Rect(
-                visibleBounds.left.toFloat(),
-                visibleBounds.top.toFloat(),
-                visibleBounds.right.toFloat(),
-                visibleBounds.bottom.toFloat()
-            )
-        ).isEmpty
+        !nodeBoundsInWindow.intersect(visibleBounds).isEmpty
     }
 }
 

compose/ui/ui-test/src/skikoMain/kotlin/androidx/compose/ui/test/ComposeUiTest.skikoMain.kt
@@ -28,9 +28,9 @@ import androidx.compose.ui.node.RootForTest
 import androidx.compose.ui.platform.InfiniteAnimationPolicy
 import androidx.compose.ui.platform.PlatformContext
 import androidx.compose.ui.platform.WindowInfo
-import androidx.compose.ui.scene.MultiLayerComposeScene
 import androidx.compose.ui.scene.ComposeScene
 import androidx.compose.ui.scene.ComposeSceneContext
+import androidx.compose.ui.scene.MultiLayerComposeScene
 import androidx.compose.ui.semantics.SemanticsNode
 import androidx.compose.ui.text.input.EditCommand
 import androidx.compose.ui.text.input.ImeAction
@@ -39,7 +39,6 @@ import androidx.compose.ui.text.input.PlatformTextInputService
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.toIntRect
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.coroutines.cancellation.CancellationException
@@ -178,12 +177,11 @@ class SkikoComposeUiTest(
 
     private fun createUi() = MultiLayerComposeScene(
         density = density,
+        size = size,
         coroutineContext = coroutineContext,
         composeSceneContext = TestComposeSceneContext(),
         invalidate = { }
-    ).also {
-        it.boundsInWindow = size.toIntRect()
-    }
+    )
 
     private fun shouldPumpTime(): Boolean {
         return mainClock.autoAdvance &&

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/platform/PlatformWindowContext.desktop.kt
@@ -19,7 +19,10 @@ package androidx.compose.ui.platform
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.input.pointer.PointerKeyboardModifiers
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.window.Dialog
@@ -28,11 +31,10 @@ import java.awt.Component
 import java.awt.Container
 import java.awt.Point
 import javax.swing.SwingUtilities
+import kotlin.math.roundToInt
 
 /**
  * Tracking a state of window.
- *
- * TODO: Extract information about window from [PlatformContext] in skiko source set.
  */
 internal class PlatformWindowContext {
     private val _windowInfo = WindowInfoImpl()
@@ -58,29 +60,39 @@ internal class PlatformWindowContext {
         _windowContainer = windowContainer
     }
 
-    fun setContainerSize(size: IntSize) {
-        if (_windowInfo.containerSize != size) {
-            _windowInfo.containerSize = size
-        }
+    fun setContainerSize(size: Size) {
+        _windowInfo.containerSize = IntSize(
+            width = size.width.roundToInt(),
+            height = size.height.roundToInt()
+        )
     }
+
+    fun calculatePositionInWindow(container: Component, localPosition: Offset) =
+        localPosition + offsetInWindow(container).toOffset(container.density)
+
+    fun calculateLocalPosition(container: Component, positionInWindow: Offset) =
+        positionInWindow - offsetInWindow(container).toOffset(container.density)
 
     /**
      * Calculates the offset of the given [container] within the window.
      * It uses [_windowContainer] as a reference for window coordinate space.
      *
      * @param container The container component whose offset needs to be calculated.
-     * @return The offset of the container within the window as an [IntOffset] object.
+     * @return The offset of the container within the window as an [Point] object.
      */
-    fun offsetInWindow(container: Component): IntOffset {
-        val scale = container.density.density
-        val pointInWindow = if (_windowContainer != null) {
+    fun offsetInWindow(container: Component): Point {
+        return if (_windowContainer != null) {
             SwingUtilities.convertPoint(container, Point(0, 0), _windowContainer)
         } else {
             Point(0, 0)
         }
-        return IntOffset(
-            x = (pointInWindow.x * scale).toInt(),
-            y = (pointInWindow.y * scale).toInt()
-        )
     }
+}
+
+private fun Point.toOffset(density: Density): Offset {
+    val scale = density.density
+    return Offset(
+        x = x * scale,
+        y = y * scale
+    )
 }

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/scene/ComposeContainer.desktop.kt
@@ -102,7 +102,8 @@ internal class ComposeContainer(
             _windowContainer = value
 
             windowContext.setWindowContainer(value)
-            onChangeWindowBounds()
+            onChangeWindowSize()
+            onChangeWindowPosition()
         }
 
     private val coroutineExceptionHandler = DesktopCoroutineExceptionHandler()
@@ -146,8 +147,8 @@ internal class ComposeContainer(
         layers.fastForEach(DesktopComposeSceneLayer::close)
     }
 
-    override fun componentResized(e: ComponentEvent?) = onChangeWindowBounds()
-    override fun componentMoved(e: ComponentEvent?) = onChangeWindowBounds()
+    override fun componentResized(e: ComponentEvent?) = onChangeWindowPosition()
+    override fun componentMoved(e: ComponentEvent?) = onChangeWindowSize()
     override fun componentShown(e: ComponentEvent?) = Unit
     override fun componentHidden(e: ComponentEvent?) = Unit
 
@@ -160,11 +161,18 @@ internal class ComposeContainer(
         layers.fastForEach(DesktopComposeSceneLayer::onChangeWindowFocus)
     }
 
-    private fun onChangeWindowBounds() {
+    private fun onChangeWindowPosition() {
+        if (!container.isDisplayable) return
+
+        mediator.onChangeComponentPosition()
+    }
+
+    private fun onChangeWindowSize() {
         if (!container.isDisplayable) return
 
         windowContext.setContainerSize(windowContainer.sizeInPx)
-        layers.fastForEach(DesktopComposeSceneLayer::onChangeWindowBounds)
+        mediator.onChangeComponentSize()
+        layers.fastForEach(DesktopComposeSceneLayer::onChangeWindowSize)
     }
 
     fun onChangeWindowTransparency(value: Boolean) {
@@ -187,7 +195,8 @@ internal class ComposeContainer(
         setWindow(SwingUtilities.getWindowAncestor(container))
 
         // Re-checking the actual size if it wasn't available during init.
-        onChangeWindowBounds()
+        onChangeWindowSize()
+        onChangeWindowPosition()
     }
 
     fun removeNotify() {
@@ -203,7 +212,8 @@ internal class ComposeContainer(
 
         // In case of preferred size there is no separate event for changing window size,
         // so re-checking the actual size on container resize too.
-        onChangeWindowBounds()
+        onChangeWindowSize()
+        onChangeWindowPosition()
     }
 
     private fun setWindow(window: Window?) {
@@ -246,22 +256,22 @@ internal class ComposeContainer(
         return when (layerType) {
             LayerType.OnSameCanvas ->
                 MultiLayerComposeScene(
+                    density = density,
+                    layoutDirection = layoutDirection,
                     coroutineContext = mediator.coroutineContext,
                     composeSceneContext = createComposeSceneContext(
                         platformContext = mediator.platformContext
                     ),
-                    density = density,
                     invalidate = mediator::onComposeInvalidation,
-                    layoutDirection = layoutDirection,
                 )
             else -> SingleLayerComposeScene(
-                coroutineContext = mediator.coroutineContext,
                 density = density,
-                invalidate = mediator::onComposeInvalidation,
                 layoutDirection = layoutDirection,
+                coroutineContext = mediator.coroutineContext,
                 composeSceneContext = createComposeSceneContext(
                     platformContext = mediator.platformContext
                 ),
+                invalidate = mediator::onComposeInvalidation,
             )
         }
     }

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/scene/ComposeSceneMediator.desktop.kt
@@ -23,33 +23,65 @@ import androidx.compose.ui.ComposeFeatureFlags
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.asComposeCanvas
-import androidx.compose.ui.input.pointer.*
-import androidx.compose.ui.platform.*
+import androidx.compose.ui.input.pointer.AwtCursor
+import androidx.compose.ui.input.pointer.PointerButton
+import androidx.compose.ui.input.pointer.PointerButtons
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.PointerKeyboardModifiers
+import androidx.compose.ui.input.pointer.PointerType
+import androidx.compose.ui.platform.AccessibilityController
+import androidx.compose.ui.platform.ComposeSceneAccessible
+import androidx.compose.ui.platform.DelegateRootForTestListener
+import androidx.compose.ui.platform.DesktopTextInputService
+import androidx.compose.ui.platform.EmptyViewConfiguration
+import androidx.compose.ui.platform.PlatformComponent
+import androidx.compose.ui.platform.PlatformContext
+import androidx.compose.ui.platform.PlatformWindowContext
+import androidx.compose.ui.platform.ViewConfiguration
+import androidx.compose.ui.platform.WindowInfo
 import androidx.compose.ui.scene.skia.SkiaLayerComponent
 import androidx.compose.ui.semantics.SemanticsOwner
 import androidx.compose.ui.text.input.PlatformTextInputService
 import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntRect
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toOffset
 import androidx.compose.ui.window.WindowExceptionHandler
 import androidx.compose.ui.window.density
 import androidx.compose.ui.window.sizeInPx
-import java.awt.*
+import java.awt.Component
 import java.awt.Cursor
-import java.awt.event.*
+import java.awt.Dimension
+import java.awt.Point
+import java.awt.Toolkit
+import java.awt.event.ContainerEvent
+import java.awt.event.ContainerListener
+import java.awt.event.FocusEvent
+import java.awt.event.FocusListener
+import java.awt.event.InputEvent
+import java.awt.event.InputMethodEvent
+import java.awt.event.InputMethodListener
+import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
+import java.awt.event.MouseWheelEvent
 import java.awt.im.InputMethodRequests
 import javax.accessibility.Accessible
 import javax.swing.JLayeredPane
 import javax.swing.SwingUtilities
 import kotlin.coroutines.CoroutineContext
+import kotlin.math.roundToInt
 import org.jetbrains.skia.Canvas
-import org.jetbrains.skiko.*
+import org.jetbrains.skiko.ClipComponent
+import org.jetbrains.skiko.ExperimentalSkikoApi
+import org.jetbrains.skiko.GraphicsApi
+import org.jetbrains.skiko.SkikoInput
+import org.jetbrains.skiko.SkikoView
+import org.jetbrains.skiko.hostOs
 import org.jetbrains.skiko.swing.SkiaSwingLayer
 
 /**
@@ -182,7 +214,15 @@ internal class ComposeSceneMediator(
      * For example if we want to show dialog in a separate window with size of this
      * dialog, but constrains (and scene size) should remain the size of the main window.
      */
-    var sceneBoundsInPx: IntRect? = null
+    var sceneBoundsInPx: Rect? = null
+
+    private var offsetInWindow = Point(0, 0)
+        set(value) {
+            if (field != value) {
+                field = value
+                scene.invalidatePositionInWindow()
+            }
+        }
 
     private val semanticsOwnerListener = DesktopSemanticsOwnerListener()
     var rootForTestListener: PlatformContext.RootForTestListener? by DelegateRootForTestListener()
@@ -346,7 +386,7 @@ internal class ComposeSceneMediator(
     private val MouseEvent.position: Offset
         get() {
             val pointInContainer = SwingUtilities.convertPoint(component, point, container)
-            val offset = sceneBoundsInPx?.topLeft?.toOffset() ?: Offset.Zero
+            val offset = sceneBoundsInPx?.topLeft ?: Offset.Zero
             val density = contentComponent.density
             return Offset(pointInContainer.x.toFloat(), pointInContainer.y.toFloat()) * density.density - offset
         }
@@ -464,22 +504,21 @@ internal class ComposeSceneMediator(
         skiaLayerComponent.onComposeInvalidation()
     }
 
+    fun onChangeComponentPosition() = catchExceptions {
+        if (!container.isDisplayable) return
+
+        offsetInWindow = windowContext.offsetInWindow(container)
+    }
+
     fun onChangeComponentSize() = catchExceptions {
         if (!container.isDisplayable) return
 
-        val offsetInWindow = windowContext.offsetInWindow(container)
         val size = sceneBoundsInPx?.size ?: container.sizeInPx
-        val boundsInWindow = IntRect(
-            offset = offsetInWindow,
-            size = IntSize(
-                // container.sizeInPx can be negative
-                width = size.width.coerceAtLeast(0),
-                height = size.height.coerceAtLeast(0)
-            )
+        scene.size = IntSize(
+            // container.sizeInPx can be negative
+            width = size.width.coerceAtLeast(0f).roundToInt(),
+            height = size.height.coerceAtLeast(0f).roundToInt()
         )
-        if (scene.boundsInWindow != boundsInWindow) {
-            scene.boundsInWindow = boundsInWindow
-        }
     }
 
     fun onChangeComponentDensity() = catchExceptions {
@@ -514,10 +553,10 @@ internal class ComposeSceneMediator(
         override fun onRender(canvas: Canvas, width: Int, height: Int, nanoTime: Long) {
             catchExceptions {
                 val composeCanvas = canvas.asComposeCanvas()
-                val offset = sceneBoundsInPx?.topLeft ?: IntOffset.Zero
+                val offset = sceneBoundsInPx?.topLeft ?: Offset.Zero
                 val scale = contentComponent.density.density
-                val dx = contentComponent.x * scale - offset.x.toFloat()
-                val dy = contentComponent.y * scale - offset.y.toFloat()
+                val dx = contentComponent.x * scale - offset.x
+                val dy = contentComponent.y * scale - offset.y
                 composeCanvas.translate(-dx, -dy)
                 scene.render(composeCanvas, nanoTime)
                 composeCanvas.translate(dx, dy)
@@ -595,6 +634,13 @@ internal class ComposeSceneMediator(
     private inner class DesktopPlatformContext : PlatformContext by PlatformContext.Empty {
         override val windowInfo: WindowInfo get() = windowContext.windowInfo
         override val isWindowTransparent: Boolean get() = windowContext.isWindowTransparent
+
+        override fun calculatePositionInWindow(localPosition: Offset): Offset =
+            windowContext.calculatePositionInWindow(container, localPosition)
+
+        override fun calculateLocalPosition(positionInWindow: Offset): Offset =
+            windowContext.calculateLocalPosition(container, positionInWindow)
+
         override val viewConfiguration: ViewConfiguration = DesktopViewConfiguration()
         override val textInputService: PlatformTextInputService = this@ComposeSceneMediator.textInputService
 

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/scene/DesktopComposeSceneLayer.desktop.kt
@@ -20,6 +20,6 @@ internal abstract class DesktopComposeSceneLayer : ComposeSceneLayer {
     open fun onChangeWindowFocus() {
     }
 
-    open fun onChangeWindowBounds() {
+    open fun onChangeWindowSize() {
     }
 }

compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/scene/SwingComposeSceneLayer.desktop.kt
@@ -202,7 +202,7 @@ internal class SwingComposeSceneLayer(
         )
     }
 
-    override fun onChangeWindowBounds() {
+    override fun onChangeWindowSize() {
         containerSize = IntSize(windowContainer.width, windowContainer.height)
     }
 

commit 0ce5274da2332170b65207cdb3a6d1519a12f73d

Fix `CGPoint` passing during position calculation (#1144)
## Proposed Changes

- Fix convert point iOS issue (regression after #1117)

## Testing

Test: check position of text selection handles

compose/ui/ui/src/uikitMain/kotlin/androidx/compose/ui/platform/PlatformWindowContext.uikit.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.unit.asCGPoint
 import androidx.compose.ui.unit.asDpOffset
 import androidx.compose.ui.unit.toDpOffset
 import androidx.compose.ui.unit.toOffset
-import kotlinx.cinterop.cValue
 import kotlinx.cinterop.useContents
 import platform.UIKit.UIView
 
@@ -70,7 +69,7 @@ internal class PlatformWindowContext {
         return if (fromView != toView) {
             val density = fromView.systemDensity
             fromView.convertPoint(
-                point = cValue { point.toDpOffset(density).asCGPoint() },
+                point = point.toDpOffset(density).asCGPoint(),
                 toView = toView,
             ).useContents {
                 asDpOffset().toOffset(density)